894e270f9b0be08b296061faeecf6b7f0220f4e6
yeonhee <cyh6327@gmail.com>
2022-10-30 16:33:26 +0900

    백준1000

---
+++ 백준1000.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		int A = 0, B = 0;
+		Scanner scanner = new Scanner(System.in);
+		A = scanner.nextInt();
+		Scanner scanner1 = new Scanner(System.in);
+		B = scanner.nextInt();
+		System.out.println(A+B);
+	}
+}
