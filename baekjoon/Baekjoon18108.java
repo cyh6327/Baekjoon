59196a7d03892afdab0497a6a3ded3abd40ab9a1
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2022-11-19 23:57:21 +0900

    Baekjoon18108

---
+++ baekjoon/Baekjoon18108.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon18108 {
+	public static void main(String[] args) {
+		int num = 0;
+		Scanner scanner = new Scanner(System.in);
+		num = scanner.nextInt();
+		System.out.println(num-(2541-1998));
+	}
+}
