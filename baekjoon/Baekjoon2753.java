e3fb18dbd7c3f4a6dd9bef51b993d5ce986098dd
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2022-12-14 23:33:57 +0900

    Baekjoon2753

---
+++ baekjoon/Baekjoon2753.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon2753 {
+	public static void main(String[] args) {
+			
+			Scanner scanner = new Scanner(System.in);
+			int num = scanner.nextInt();
+			
+			if(num%4 == 0 && num%100 != 0 || num%400 == 0)
+				System.out.println("1");
+			else
+				System.out.println("0");
+	
+		}
+}
