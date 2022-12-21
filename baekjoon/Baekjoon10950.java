f9d974d19c572129b6c41cd462c4f5f688f3e6a7
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2022-12-21 21:50:16 +0900

    Baekjoon10950

---
+++ baekjoon/Baekjoon10950.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon10950 {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		int sum = 0;
+		for(int i=0; i<t; i++) {
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			System.out.println(a+b);			
+		}
+	}
+
+}
