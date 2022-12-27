2677e0916f9db0c46d575c87f07277ed202d0c9e
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2022-12-27 23:53:18 +0900

    Backjoon25304

---
+++ baekjoon/Backjoon25304.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Backjoon25304 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int X = scanner.nextInt();
+		int N = scanner.nextInt();
+		int a;
+		int b;
+		
+		int sum = 0;
+		
+		for(int i=0; i<N; i++) {
+			a = scanner.nextInt();
+			b = scanner.nextInt();
+			sum += a*b;
+		}
+		if(X == sum)
+			System.out.println("Yes");
+		else
+			System.out.println("No");
+	}
+	
+}
