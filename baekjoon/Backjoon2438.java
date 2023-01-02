687992b170ee72747457c5639230b25c3910a2bf
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2023-01-02 17:17:41 +0900

    Backjoon2438

--- baekjoon/Backjoon2438.java
+++ baekjoon/Backjoon2438.java
@@ -1,5 +1,19 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Backjoon2438 {
+	
+public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			for(int a=1; a<=i ; a++)
+				System.out.print("*");
+			System.out.println();
+		}
+	}
 
 }
