7d7ce0495ac494cd1999468d5fe709f33c0043a0
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2023-01-05 17:38:22 +0900

    Backjoon11022

--- baekjoon/Backjoon11021.java
+++ baekjoon/Backjoon11021.java
@@ -1,5 +1,23 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Backjoon11021 {
+	
+public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		int a;
+		int b;
+		int sum = 0;
+		
+		for(int i=1; i<=t; i++) {
+			a = scanner.nextInt();
+			b = scanner.nextInt();
+			sum = a + b;
+			System.out.println("Case #" +i+ ": "+ sum);
+		}
+	}
 
 }
