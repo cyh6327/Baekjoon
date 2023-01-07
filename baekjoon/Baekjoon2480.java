59dbed93e31060ce18ac8e17cf26a472bedf72ce
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2023-01-07 23:48:42 +0900

    Baekjoon2480

--- baekjoon/Baekjoon2480.java
+++ baekjoon/Baekjoon2480.java
@@ -1,5 +1,36 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Baekjoon2480 {
+	
+public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+
+		int money=0;
+
+		if(a==b && c==a) 
+			money = 10000 + (a*1000);
+		else if(a == b && a != c)
+			money = 1000 + (a*100);
+		else if(a == c && a != b)
+			money = 1000 + (a*100);
+		else if(b == c && b != a)
+			money = 1000 + (b*100);
+		else {
+			if(a>b && a>c)
+				money = a*100;
+			else if(b>a && b>c)
+				money = b*100;
+			else
+				money = c*100;
+		}
+		
+		System.out.println(money);
+	}
 
 }
