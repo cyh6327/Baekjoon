78ed3f7dcec5ba62746f85d360c2884ef615873b
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2023-02-21 00:23:16 +0900

    Baekjoon11382 꼬마 정민

---
+++ baekjoon/Baekjoon11382.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon11382 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		long a = scan.nextLong();
+		long b = scan.nextLong();
+		long c = scan.nextLong();
+		System.out.println(a+b+c);
+		scan.close();
+	}
+
+}
