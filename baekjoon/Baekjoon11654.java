410154d05ddcd6777c5aa37e4c744bbb3b92af81
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2023-03-02 20:29:49 +0900

    Baekjoon11654 아스키 코드

---
+++ baekjoon/Baekjoon11654.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon11654 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		char c = scan.next().charAt(0);
+		System.out.println((int)c);
+		
+		scan.close();
+	}
+
+}
