7657e84d230f9859b714705b2911ebb36ff0f2b2
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2023-03-03 16:04:46 +0900

    Baekjoon2743 단어 길이 재기

---
+++ baekjoon/Baekjoon2743.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon2743 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String str = scan.next();
+		System.out.println(str.length());
+		
+		scan.close();
+	}
+
+}
