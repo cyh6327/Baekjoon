e49bedf0fda5bf47fb681e833b762e002a397bec
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2023-03-15 22:36:59 +0900

    Baekjoon11718 그대로 출력하기

---
+++ baekjoon/Baekjoon11718.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon11718 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		String str;
+		
+		while(scan.hasNext()) {
+			str = scan.nextLine();
+			System.out.println(str);
+		}
+		
+		scan.close();
+	}
+
+}
