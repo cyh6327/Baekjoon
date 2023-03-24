b8011bc715b9b4e3192a1c46028c23eb22992a95
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2023-03-24 22:29:13 +0900

    Baekjoon2558 A+B - 2

---
+++ baekjoon/Baekjoon2558.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon2558 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		System.out.println(a+b);
+		
+		scan.close();
+	}
+
+}
