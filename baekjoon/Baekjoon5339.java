bb95ead42b04ed1d5ce1786b3789f54273bbefd6
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2023-03-30 23:37:02 +0900

    Baekjoon5339 콜센터

---
+++ baekjoon/Baekjoon5339.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+public class Baekjoon5339 {
+	
+	public static void main(String[] args) {
+		System.out.println("     /~"+"\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _"+"\\"+"=/"+"_");
+		System.out.println("   /  _  "+"\\");
+		System.out.println("  //|/."+"\\"+"|"+"\\\\");
+		System.out.println(" ||  "+"\\"+" /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+	}
+
+}
