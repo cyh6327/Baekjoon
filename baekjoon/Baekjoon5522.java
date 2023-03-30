6c8211b0cbfe7f93309a2cf501771e98ac0c057e
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2023-03-30 23:49:55 +0900

    Baekjoon5522 카드 게임

---
+++ baekjoon/Baekjoon5522.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon5522 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int a = 0;
+		int sum = 0;
+		
+		for(int i=0; i<5; i++) {
+			a = scan.nextInt();
+			sum += a;
+		}
+		
+		System.out.println(sum);
+		
+		scan.close();
+	}
+}
