b2c5a3b85459963ca1bec60b2f66bdee558a1229
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2023-03-31 09:46:30 +0900

    Baekjoon6840 Who is in the middle?

---
+++ baekjoon/Baekjoon6840.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Baekjoon6840 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int[] arr = new int[3];
+		
+		for(int i=0; i<3; i++) {
+			arr[i] = scan.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[1]);
+		
+		scan.close();
+	}
+
+}
