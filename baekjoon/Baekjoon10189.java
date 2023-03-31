195cca687b6df4e273c7fd9fe191266dc9ca0160
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2023-04-01 08:24:36 +0900

    Baekjoon10189 Hook

---
+++ baekjoon/Baekjoon10189.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+public class Baekjoon10189 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("#  # #### #### #  #"+"\n"+
+							"#### #  # #  # # #"+"\n"+
+							"#### #  # #  # # #"+"\n"+
+							"#  # #### #### #  #");
+	}
+}
