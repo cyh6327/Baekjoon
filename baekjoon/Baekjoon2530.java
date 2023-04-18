020e4212ec37fd36198073b34ad7445bacba7cbf
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2023-04-18 23:57:57 +0900

    Baekjoon2530 인공지능 시계

---
+++ baekjoon/Baekjoon2530.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon2530 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		
+		
+		scan.close();
+	}
+
+}
