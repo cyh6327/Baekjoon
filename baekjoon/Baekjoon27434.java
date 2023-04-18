5c00b9f832357de058742ab010cc6b4fd025b925
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2023-04-18 23:54:09 +0900

    Baekjoon27434 팩토리얼 3 (미결)

---
+++ baekjoon/Baekjoon27434.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Baekjoon27434 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+
+		//예제 입력 10에 대해서는 정상 출력되지만 제출하면 시간 초과,,
+//		BigInteger f = new BigInteger("1");
+//		
+//		for(int i=1; i<=n; i++) {
+//			f = f.multiply(BigInteger.valueOf(i));
+//		}
+//		
+//		System.out.println(f);
+		
+		scan.close();
+	}
+
+}
