994bcc22153b20ecb2ceea60389f346bb3895df4
DESKTOP-739CV56\cyh63 <cyh6327@gmail.com>
2023-04-22 23:23:47 +0900

    Baekjoon8871 Zadanie próbne 2

---
+++ baekjoon/Baekjoon8871.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon8871 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		System.out.println(((n+1)*2) + " " + ((n+1)*3));
+		
+		scan.close();
+	}
+
+}
