701e8c1387351bed7612fc8b9c8ce522851142a6
cyh6327 <cyh6327@gmail.com>
2023-12-29 10:46:01 +0900

    Baekjoon2446 별 찍기 - 9

---
+++ baekjoon/Baekjoon2446.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon2446 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<=i; j++) {
+                if(j==i) break;
+                System.out.print(" ");
+            }
+            for(int k=2*n; k>2*i+1; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=0; i<n-1; i++) {
+            for(int j=n-2; j>i; j--) {
+                System.out.print(" ");
+            }
+            for(int k=0; k<i*2+3; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
